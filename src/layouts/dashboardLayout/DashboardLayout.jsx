ae1f97b4ce1f719db50de2b54c27683515cbc4c7
jinsek <jinsek@cs.washington.edu>
2024-12-27 13:32:55 -0800

    Homepage and until login

--- src/layouts/dashboardLayout/DashboardLayout.jsx
+++ src/layouts/dashboardLayout/DashboardLayout.jsx
@@ -1,10 +1,28 @@
+import { useAuth } from '@clerk/clerk-react'
 import './dashboardLayout.css'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react';
+import ChatList from '../../components/chatList/ChatList';
 
 const DashboardLayout = () => {
+
+  const {userID, isLoaded} = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLoaded && !userID) {
+    navigate('/sign-in');
+    }
+    // maybe add isLoaded to the dependency array
+  }, [userID, navigate]);
+
+  if (!isLoaded) {
+    return 'Loading...';
+  }
+
   return (
     <div className='dashboardLayout'>
-      <div className="menu">MENU</div>
+      <div className="menu"><ChatList /></div>
       <div className="content"><Outlet /></div>
     </div>
   )
--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -1,8 +1,70 @@
+import { Link } from 'react-router-dom'
 import './homepage.css'
+import { TypeAnimation } from 'react-type-animation'
+import { useState } from 'react';
 
 const Homepage = () => {
+
+  const [typingStatus, setTypingStatus] = useState('Human1');
   return (
-    <div className='homepage'>Homepage</div>
+    <div className='homepage'>
+      <img src="/orbital.png" alt="" className='orbital'/>
+      <div className='left'>
+        <h1>JK AI</h1>
+        <h2>Making Tomorrow Today with AI</h2>
+        <h3>
+          JK AI is a powerful generative AI tool to help you create amazing content.
+          Expand your creativity with a touch of AI.
+        </h3>
+        <Link to='/dashboard'>Get Started</Link>
+      </div>
+      <div className='right'>
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="/bot2.png" alt="" />
+          <div className='typeChats'>
+            <img src={typingStatus === "Human1" ? "/human1.jpeg" :
+                      typingStatus === "Human2" ? "/human2.jpeg" : "/bot2.png"
+            } alt="" />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'Human: We produce food for Mice',
+                2000, () => {
+                  setTypingStatus('Bot');
+                },
+                'Bot: We produce food for Hamsters',
+                2000, () => {
+                  setTypingStatus('Human2');
+                },
+                'Human2: We produce food for Guinea Pigs',
+                2000, () => {
+                  setTypingStatus('Bot');
+                },
+                'Bot: We produce food for Chinchillas',
+                2000, () => {
+                  setTypingStatus('Human1');
+                },
+              ]}
+              wrapper="span"
+              repeat={Infinity}
+              cursor={true}
+              omitDeletionAnimation={true}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="terms">
+        <img src="/logo.png" alt="" />
+        <div className="links">
+          <Link to='/'>Terms of Service</Link>
+          <span>|</span>
+          <Link to='/'>Privacy Policy</Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
