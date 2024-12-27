c53b3b4400c48874540005bf221421a9a4025701
jinsek <jinsek@cs.washington.edu>
2024-12-27 15:29:36 -0800

    something more idc

--- src/components/chatList/ChatList.jsx
+++ src/components/chatList/ChatList.jsx
@@ -1,8 +1,44 @@
+import { Link } from 'react-router-dom'
 import './chatList.css'
 
 const ChatList = () => {
   return (
-    <div className='chatList'>ChatList</div>
+    <div className='chatList'>
+      <span className="title">DASHBOARD</span>
+      <Link to='/dashboard'>Create a new Chat</Link>
+      <Link to='/'>Explore JK AI</Link>
+      <Link to='/'>Contact</Link>
+      <hr />
+      <span className='title'>RECENT CHATS</span>
+      <div className="list">
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+        <Link to='/'>My Chat title</Link>
+      </div>
+      <hr />
+      <div className="upgrade">
+        <img src="/logo.png" alt="" />
+        <div className="texts">
+          <span>Upgrade to JK AI Pro</span>
+          <span>Get unlimited access to all features</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,8 +1,41 @@
+import { useEffect, useRef } from 'react'
 import './chatpage.css'
+import NewPrompt from '../../components/newPrompt/NewPrompt';
 
 const ChatPage = () => {
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current.scrollIntoView({ behavior: 'smooth' });
+  }, []);
+
   return (
-    <div className='chatPage'>ChatPage</div>
+    <div className='chatPage'>
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test Message</div>
+          <div className="message user">Test Message from Usersest Message from Usersest Message from Usersest Message from Usersest Message from Usersest Message from Usersest Message from Users</div>
+          <div className="message">Test Message</div>
+          <div className="message user">Test Message from Users</div>
+          <div className="message">Test Message</div>
+          <div className="message user">Test Message from Usersest Message from Usersest Message from Usersest Message from Usersest Message from Usersest Message from Usersest Message from Users</div>
+          <div className="message">Test Message</div>
+          <div className="message user">Test Message from Users</div>
+          <div className="message">Test Message</div>
+          <div className="message user">Test Message from Usersest Message from Usersest Message from Usersest Message from Usersest Message from Usersest Message from Usersest Message from Users</div>
+          <div className="message">Test Message</div>
+          <div className="message user">Test Message from Users</div>
+          <div className="message">Test Message</div>
+          <div className="message user">Test Message from Users</div>
+          <div className="message">Test Message</div>
+          <div className="message user">Test Message from Usersest Message from Usersest Message from Usersest Message from Usersest Message from Usersest Message from Usersest Message from Users</div>
+          <div className="message">Test Message</div>
+          <div className="message user">Test Message from Users</div>
+          <NewPrompt />
+          <div ref={endRef}></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
